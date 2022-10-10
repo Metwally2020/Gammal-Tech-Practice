fdc3c9f0e901efa637b0dabc13ce15288694ecec
‪Mahmoud Metwally‬‏ <mahmoud.metwally007@gmail.com>
2022-10-10 19:22:01 +0200

    onboarding added

--- lib/onboarding/onboarding_contents.dart
+++ lib/onboarding/onboarding_contents.dart
@@ -10,18 +10,18 @@ class OnboardingContents {
 List<OnboardingContents> contents = [
   OnboardingContents(
     title: "Track Your progress and get the result",
-    image: "assets/images/image1.png",
+    image: "assets/image1.png",
     desc: "Remember to keep track of your professional accomplishments.",
   ),
   OnboardingContents(
     title: "Practice your coding skills",
-    image: "assets/images/image2.png",
+    image: "assets/image2.png",
     desc:
         "But understanding the contributions our colleagues make to our teams and companies.",
   ),
   OnboardingContents(
     title: "Trace problems and find solutions",
-    image: "assets/images/image3.png",
+    image: "assets/image3.png",
     desc:
         "Take control of notifications, collaborate live or on your own time.",
   ),
